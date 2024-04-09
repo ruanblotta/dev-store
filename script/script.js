712831f285a4482e61535cf68ade9ada1034fae2
Ruan Blotta <ruanblotta@gmail.com>
2024-04-09 20:44:45 -0300

    Implementando scrip do produto

--- script/script.js
+++ script/script.js
@@ -5,14 +5,34 @@ fetch(URL)
     .then(data => {
         const products = data.results;
 
-        const productList = document.createElement('ul');
-        products.forEach(product => {
-            const listItem = document.createElement('li');
-            listItem.textContent = product.title;
-            productList.appendChild(listItem);
-        });
+        // selecionar o elemento onde os produtos serão renderizados
+        const productListContainer = document.getElementById('product-list');
 
-        document.getElementById('product-list').appendChild(productList);
+        //criar uma div para cada produto e renderizar os produtos dentre delas
+        products.forEach(product => {
+            //criar uma div para o produto
+            const productDiv = document.createElement('div');
+            productDiv.classList.add('product'); //adiciona uma classe para estilização
+
+            //criar imagem do produto
+            const image = document.createElement('img');
+            image.src = product.thumbnail; 
+            image.alt = product.title;
+            productDiv.appendChield(image);
+
+            // criar titulo do produto
+            const title = document. createElement('span');
+            title.textContent = product.title;
+            productDiv.appendChild(title);
+
+            // criar preço do produto
+            const price = document.createElement('span');
+            price.textConent = ' - R$ ' + product.price.toFixed(2); // formatando o preço
+            productDiv.appendChield(price);
+
+            // adicionar a div do produto ao conteiner principal
+            productListContainer.appendChild(productDiv);
+        });
     })
     .catch(error => {
         console.error('Erro ao obter os dados:', error);
